ddd404e5ad10db8b3be03247f98518888371a5a0
goldim1993 <goldim1993@gmail.com>
2022-12-22 18:47:27 +0300

    Added and implemented new properties: minLabel, autofocus. Code divided on private methods. Click and tap event instead of focus.

--- source/class/ugpa/completer/Completer.js
+++ source/class/ugpa/completer/Completer.js
@@ -8,7 +8,6 @@ qx.Class.define("ugpa.completer.Completer", {
         this.initPopup(new qx.ui.menu.Menu());
         this.initWidget(widget);
         this.__filterFunc = this.__getFilterModeFunc();
-        this.__oneAgainAfterFirstInput = false;
     },
 
     events: {
@@ -17,9 +16,18 @@ qx.Class.define("ugpa.completer.Completer", {
     },
 
     properties: {
+        autofocus: {
+            init: false,
+            check: "Boolean"
+        },
+
         caseSensitivity: {
             init: true,
             check: "Boolean"
+        },
+
+        minLength: {
+            init: 1
         },
 
         maxVisibleItems: {
@@ -46,8 +54,8 @@ qx.Class.define("ugpa.completer.Completer", {
     members: {
         _applyWidget(widget){
             widget.addListener("input", this._onInput, this);
-            widget.addListener("focus", this._onFocus, this);
-            widget.addListener("focusout", this._onFocusOut, this);
+            widget.addListener("click", this._onFocus, this);
+            widget.addListener("tap", this._onFocus, this);
         },
 
         _applyFilterMode(){
@@ -55,45 +63,72 @@ qx.Class.define("ugpa.completer.Completer", {
         },
 
         _onFocus(){
+            const value = this.getWidget().getValue();
+            if (!value && this.getMinLength() > 0){
+                return;
+            }
             const popup = this.getPopup();
-            popup.setVisibility("visible");
+            popup.show();
             popup.placeToWidget(this.getWidget());
 
-            const value = this.getWidget().getValue();
             this.__applyInput(value === null ? "" : value);
-        },
-
-        _onFocusOut(){
-            console.log("FOCUS OUT");
         },
 
         _onInput(e){
             const input = e.getData();
-            if (this.__oneAgainAfterFirstInput){
-                this.getPopup().setVisibility("visible");
+            if (input.length < this.getMinLength()) {
+                if (this.getPopup().getVisibility() === "visible"){
+                    this.getPopup().hide();
+                }
+                return;
             }
+
+            this.getPopup().show();
+            this.getPopup().placeToWidget(this.getWidget());
             this.__applyInput(input);
         },
 
+        __applyAutofocus(){
+            if (this.getAutofocus()){
+                const popup = this.getPopup();
+                const firstButton = popup.getSelectables()[0];
+                if (firstButton){
+                    popup.setSelectedButton(firstButton);
+                }
+            }
+        },
+
         __applyInput(input){
+            this.__clearPopup();
+            const values = this.__getValuesFromSourceByWord(input);
+            values.slice(0, this.getMaxVisibleItems()).forEach(this.__addItemOnPopup, this);
+            this.__applyAutofocus();
+        },
+
+        __addItemOnPopup(value){
+            const button = new qx.ui.menu.Button(value);
+            button.addListener("execute", this._onItemPressed, this);
+            this.getPopup().add(button);
+        },
+
+        _onItemPressed(e){
+            const button = e.getTarget();
+            this.getWidget().setValue(button.getLabel());
+        },
+
+        __clearPopup(){
             const popup = this.getPopup();
             popup.removeAll();
+        },
 
+        __getValuesFromSourceByWord(input){
             let values;
             if (!this.getCaseSensitivity()){
                 values = this.__filterCaseInsensitiveValues(input);
             } else {
                 values = this.__filterCaseSensitiveValues(input);
             }
-
-            values.slice(0, this.getMaxVisibleItems()).forEach(value => {
-                const button = new qx.ui.menu.Button(value);
-                button.addListener("execute", function(){
-                    this.getWidget().setValue(value);
-                    this.__oneAgainAfterFirstInput = true;
-                }, this);
-                popup.add(button);
-            });
+            return values;
         },
 
         __filterCaseInsensitiveValues(input){
